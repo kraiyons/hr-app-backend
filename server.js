ef1e5ce4dec20f6d36abf077d0a438ca5d4a6611
Krai Remle Alapide <krai.alapide@gmail.com>
2021-08-10 23:04:41 +0800

    Added mongoose

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const { PORT, MONGO_DB } = process.env;
 
 app.use(express.json());
-const port = 5000;
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(MONGO_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Succesfully connected to the database');
+  })
+  .catch((err) => {
+    console.log('Could not connect to the database. Exiting now...', err);
+
+    process.exit();
+  });
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
